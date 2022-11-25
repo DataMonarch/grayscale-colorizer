c8ebf93244f9ced69417120b8255dd5ca7ac8c79
DataMonarch <toghrultahirov2@gmail.com>
2022-11-25 12:12:34 +0400

    Initial Commit

---
+++ qs.java
@@ -0,0 +1,18 @@
+class QuickStart {
+    /**
+     * @param args
+     */
+    public static void main (String[] args) {
+
+        String filename = args[0];
+        int sq_size = Integer.parseInt(args[1]);
+        String processing_mode = args[2];
+
+        System.out.println(filename);
+        System.out.println(sq_size);
+        System.out.println(processing_mode);
+
+         
+    }
+}
+
